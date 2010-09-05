3215855555f6cb72e532e844323ee7a1b78b63e2
Felipe Doria <felipe.doria@gmail.com>
2010-09-05 20:13:14 -0300

    Default values for tweet parsing

--- lib/translate_it/core.rb
+++ lib/translate_it/core.rb
@@ -4,11 +4,13 @@ module TranslateIt
   extend self
   
   def parse_tweet(tweet)
+    options = {:from => "", :to => "english"}
     tweet.slice!('@translate_it')
-    output = {}
-    output[:from] = tweet.slice!(/from\s+(\w+)/).slice!(/from\s+(\w+)/).slice(/from\s+(\w+)/, 1)
-    output[:to] = tweet.slice!(/to\s+(\w+)/).slice(/to\s+(\w+)/, 1)
-    output[:q] = tweet.squeeze(" ").strip
-    output
+    from = tweet.slice!(/from\s+(\w+)/)
+    to   = tweet.slice!(/to\s+(\w+)/)
+    options[:from] = from.slice(/from\s+(\w+)/, 1) if from
+    options[:to]   = to.slice(/to\s+(\w+)/, 1) if to
+    options[:q]    = tweet.squeeze(" ").strip
+    options
   end
 end
--- test/translate_it/core_test.rb
+++ test/translate_it/core_test.rb
@@ -8,20 +8,40 @@ require "contest"
 
 class CoreTest < Test::Unit::TestCase
   describe "tweet parsing" do
-    setup do
-      @result = TranslateIt.parse_tweet("@translate_it urso de pelúcia from portuguese to english")
-    end
+    describe "with lanugage pair supplied" do
+      setup do
+        @result = TranslateIt.parse_tweet("@translate_it urso de pelúcia from portuguese to english")
+      end
 
-    test "extracts the from language" do
-      assert_equal "portuguese", @result[:from]
+      test "extracts the from language" do
+        assert_equal "portuguese", @result[:from]
+      end
+    
+      test "extracts the to language" do
+        assert_equal "english", @result[:to]
+      end
+    
+      test "extracts the text query" do
+        assert_equal "urso de pelúcia", @result[:q]
+      end
     end
     
-    test "extracts the to language" do
-      assert_equal "english", @result[:to]
-    end
+    describe "pulling default values" do
+      setup do
+        @result = TranslateIt.parse_tweet("@translate_it urso de pelúcia")
+      end
+
+      test "returns default from language" do
+        assert_equal "", @result[:from]
+      end
     
-    test "extracts the text query" do
-      assert_equal "urso de pelúcia", @result[:q]
+      test "returns default to language" do
+        assert_equal "english", @result[:to]
+      end
+    
+      test "extracts the text query" do
+        assert_equal "urso de pelúcia", @result[:q]
+      end
     end
   end
 end
